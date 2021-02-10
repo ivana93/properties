5dacbb15ac29f7e2b4d9eb241b5e0040ce7b8042
ivs <ivs@ivs-something.com>
2021-02-10 22:36:35 +0200

    next commit

--- src/components/VillaComponent.js
+++ src/components/VillaComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const VillaComponent =( { villa: {name, logo, isNew, featured, postedAt, bedroom, price, location} } ) => (
+const VillaComponent =( { villa: {name, logo, isNew, featured, isUrl, postedAt, bedroom, price, location} } ) => (
     <div className="flex flex-col bg-white shadow-md my-16 mx-10 p-5 rounded-md border-l-4 border-green-500 border-solid 
     sm:flex-row sm:my-5">
     <div>
@@ -13,7 +13,7 @@ const VillaComponent =( { villa: {name, logo, isNew, featured, postedAt, bedroom
             </h3>
             <h2 className="font-bold text-xl font-sans my-2">{name}</h2>
             <p className="text-gray-600 text-sm font-mono">
-                {postedAt} · {bedroom} · {location}
+                {postedAt} · {bedroom} · {location} · <a className="font-bold text-blue-500" target="_blank" href ={isUrl}> learn more </a> 
             </p>
         </div>
         <div className="flex items-center mt-4 mx-4 pt-4 border-t-2 border-gray-200 border-solid sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0">
